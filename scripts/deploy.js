875c993db00295a42b68dd2c41c3969d628f13dd
Othmen Mhiri <othmhiri@gmail.com>
2025-08-02 13:15:46 +0100

    Added compiler project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,8 @@ const upath = require('upath');
 const distPath = upath.resolve(__dirname, '../dist');
 const docsPath = upath.resolve(__dirname, '../docs');
 
-console.log('### INFO: Cleaning docs folder');
 sh.rm('-rf', docsPath);
 sh.mkdir('-p', docsPath);
 
-console.log('### INFO: Copying files from dist to docs');
 sh.cp('-R', upath.join(distPath, '*'), docsPath);
 
-console.log('### INFO: Deployment to docs folder completed.');
